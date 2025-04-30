911c912b76e976396d4bbbc1c0a9763f748fc9dc
lolfig <agarciaflol@gmail.com>
2025-04-30 09:02:31 -0400

    Cambios que hice: - Arregle el problema de los plots locos. Estaba en que las fechas del pandas con que se pltoeaba no estaba ordenado, y por eso aparecian segmentos de un lado a otro. - Anhadi dos slots: Hidden Markov Model y Empirical mode Decomposition por lo que estos plots aparecen de forma correcta en la app.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+from matplotlib import pyplot as plt
+import pandas as pd
 import uvicorn
 
 from data_storage import data_store  # noqa
+from services.framework_scraping.time_series_fetcher import PriceTimeSeries
 
-if __name__ == '__main__':
-  print("Iniciando dashboard...")
-  uvicorn.run(
-    "app:app",
-    host='localhost',
-    port=8000
-  )
+if __name__ == "__main__":
+    # print(data_store.price_series.index)
+
+    print("Iniciando dashboard...")
+    uvicorn.run("app:app", host="localhost", port=8000)
